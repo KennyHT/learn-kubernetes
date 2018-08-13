a55825a1df67923e3de226856e11673cb23d622f
Ryan Blunden <ryan.blunden@gmail.com>
2018-08-13 15:25:39 +0100

    Labs update

--- bin/update-kubetail.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "[info]: Downloading latest kubetail script..."
-curl --silent --location --output /tmp/kubetail.tar.gz $(curl https://api.github.com/repos/johanhaleby/kubetail/releases/latest | jq -r .tarball_url)
-tar -xzf /tmp/kubetail.tar.gz -C /tmp/
-cp /tmp/johanhaleby-kubetail*/kubetail ./bin/
-unlink /tmp/kubetail.tar.gz
